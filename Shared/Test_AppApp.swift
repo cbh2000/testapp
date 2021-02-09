29fa381b0b5c72926037a9767ef42a4276a8b729
iOS Team Member <iosteam@Redlist-Mac-mini.local>
2021-02-09 11:15:26 -0700

    Added project.

---
+++ Shared/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Shared
+//
+//  Created by iOS Team Member on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
