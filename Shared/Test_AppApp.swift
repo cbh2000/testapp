1cd0cc0e64b6786fab27e86e97ddf9b70429009c
iOS Team Member <iosteam@Redlist-Mac-mini.local>
2021-02-09 17:03:10 -0700

    Added AppCenter. Removed unit tests.

--- Shared/Test_AppApp.swift
+++ Shared/Test_AppApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 struct Test_AppApp: App {
+    
+    @State var didAppear = false
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    if !didAppear {
+                        didAppear.toggle()
+                        AppCenter.start(withAppSecret: "988c348f-13a0-4b22-9752-ea26def4807b", services:[
+                          Analytics.self,
+                          Crashes.self
+                        ])
+                    }
+                }
         }
     }
 }
